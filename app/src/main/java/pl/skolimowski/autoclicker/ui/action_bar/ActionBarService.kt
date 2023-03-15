395fcb807a15447f239b49393625749f6af9992a
matskolim <matskolim@gmail.com>
2023-03-15 08:27:07 +0100

    ActionBarService *simple drag

--- app/src/main/java/pl/skolimowski/autoclicker/ui/action_bar/ActionBarService.kt
+++ app/src/main/java/pl/skolimowski/autoclicker/ui/action_bar/ActionBarService.kt
@@ -1,13 +1,14 @@
 package pl.skolimowski.autoclicker.ui.action_bar
 
 import android.accessibilityservice.AccessibilityService
+import android.annotation.SuppressLint
 import android.graphics.PixelFormat
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.WindowManager
+import android.view.*
+import android.view.MotionEvent.*
 import android.view.accessibility.AccessibilityEvent
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.flow.collectLatest
@@ -33,16 +34,19 @@ class ActionBarService : AccessibilityService() {
 
     private lateinit var viewsContainer: FrameLayout
 
+    lateinit var wm: WindowManager
+    lateinit var params: WindowManager.LayoutParams
+
     override fun onServiceConnected() {
-        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        wm = getSystemService(WINDOW_SERVICE) as WindowManager
 
         viewsContainer = FrameLayout(this)
         val inflater = LayoutInflater.from(this)
         inflater.inflate(R.layout.action_bar, viewsContainer)
 
-        val lp = createWindowLayoutParams()
+        params = createWindowLayoutParams()
 
-        wm.addView(viewsContainer, lp)
+        wm.addView(viewsContainer, params)
 
         setUpView()
     }
@@ -77,6 +81,44 @@ class ActionBarService : AccessibilityService() {
         viewsContainer.findViewById<ImageView>(R.id.iv_close).setOnClickListener {
             viewModel.onUiEvent(OnCloseImageClickedEvent)
         }
+
+        setUpDrag()
+    }
+
+    // todo move logic to view model
+    // https://stackoverflow.com/a/51361730
+    @SuppressLint("ClickableViewAccessibility") // todo check suppress
+    private fun setUpDrag() {
+        val root = viewsContainer.findViewById<LinearLayout>(R.id.root)
+
+        var initialX = 0
+        var initialY = 0
+        var initialTouchX = 0f
+        var initialTouchY = 0f
+
+        root.setOnTouchListener { view, event ->
+            when (event.action) {
+                ACTION_DOWN -> {
+                    initialX = params.x
+                    initialY = params.y
+
+                    initialTouchX = event.rawX
+                    initialTouchY = event.rawY
+
+                    return@setOnTouchListener true
+                }
+                ACTION_MOVE -> {
+                    params.x = initialX + (event.rawX - initialTouchX).toInt()
+                    params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                    wm.updateViewLayout(viewsContainer, params)
+
+                    return@setOnTouchListener true
+                }
+            }
+
+            return@setOnTouchListener false
+        }
     }
 
     private fun createWindowLayoutParams(): WindowManager.LayoutParams {
@@ -87,7 +129,6 @@ class ActionBarService : AccessibilityService() {
         lp.flags = lp.flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
         lp.width = WindowManager.LayoutParams.WRAP_CONTENT
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT
-        lp.gravity = Gravity.TOP
 
         return lp
     }
